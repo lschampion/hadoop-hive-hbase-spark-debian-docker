45d6a5ea71d164c11983f8c5654dd2c7473d1d1b
lisai <911lisa@163.com>
2022-03-03 10:46:11 +0800

    remove sparkR

--- scripts/format_hdfs.sh
+++ scripts/format_hdfs.sh
@@ -2,12 +2,12 @@
 echo "for resolving clusterID inconsistent error, ready to format hdfs"
 hdfs --daemon stop namenode
 rm -rf /dfs/name/current
-ssh worker1 "/usr/hadoop/bin/hdfs --daemon stop datanode"
+ssh worker1 "/usr/program/hadoop/bin/hdfs --daemon stop datanode"
 ssh worker1 "rm -rf /dfs/data/current"
-ssh worker2 "/usr/hadoop/bin/hdfs --daemon stop datanode"
+ssh worker2 "/usr/program/hadoop/bin/hdfs --daemon stop datanode"
 ssh worker2 "rm -rf /dfs/data/current"
 yes | hdfs namenode -format
 echo "format completed,ready restart hdfs"
 hdfs --daemon start namenode
-ssh worker1 "/usr/hadoop/bin/hdfs --daemon start datanode"
-ssh worker2 "/usr/hadoop/bin/hdfs --daemon start datanode"
+ssh worker1 "/usr/program/hadoop/bin/hdfs --daemon start datanode"
+ssh worker2 "/usr/program/hadoop/bin/hdfs --daemon start datanode"
